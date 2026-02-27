debc3c396c5fb7053648ee1203e0b99bdb9841a7
Phaniram <phaniram150@gmail.com>
2026-02-27 06:51:35 +0530

    Router fix

--- event-app/src/App.jsx
+++ event-app/src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ScrollToHash from "./components/ScrollToHash";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Admin from "./pages/Admin";
-// ❌ AdminProtectedRoute removed
 
 function App() {
   return (
@@ -19,8 +18,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
-        {/* ✅ ADMIN DIRECT ACCESS */}
         <Route path="/admin" element={<Admin />} />
       </Routes>
 
